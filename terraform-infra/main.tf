1c43612a7e282e58131598e99ed6d624d66d836d
mayurm <mayur12385@rediffmail.com>
2026-07-18 15:11:19 +0530

    changes done in main.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -1,24 +1,24 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "ntms-rg"
+  name     = "ntms-rg-mayur"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ntms-vnet"
+  name                = "ntms-vnet-mayur"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "ntms-snet"
-  resource_group_name  =   azurerm_resource_group.rg.name
+  name                 = "ntms-snet-mayur"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "ntms-nsg"
+  name                = "ntms-nsg-mayur"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
--- terraform-infra/provider.tf
+++ terraform-infra/provider.tf
@@ -9,7 +9,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "ntms-tfstate-rg"
     storage_account_name = "ntmstfstatestorage"
-    container_name       = "ayushi" #use your name - tell me the name of your container
+    container_name       = "mayur" #use your name - tell me the name of your container
     key                  = "ntms.tfstate"
   }
 }
