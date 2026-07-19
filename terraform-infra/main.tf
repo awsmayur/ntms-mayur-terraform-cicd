8857676209b2bd288dd62cc8e9cb1e6098647454
mayurm <mayur12385@rediffmail.com>
2026-07-19 14:30:27 +0530

    modified main.tf and rename variables.tf

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.environment}-${var.business_unit}-nsg"
-  location            = azurerm_resource_group.rg.location
+  location          = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
