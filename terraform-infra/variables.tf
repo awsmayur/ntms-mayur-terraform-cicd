ee8ed787d196026147043bfed3155225aaae3ffc
mayurm <mayur12385@rediffmail.com>
2026-07-19 14:35:27 +0530

    modified var.tf

--- terraform-infra/variables.tf
+++ terraform-infra/variables.tf
@@ -2,32 +2,32 @@
 
 variable "location" {
   type    = string
-  #default = "East US"
+  default = "East US"
 
 }
 
 variable "environment" {
   type    = string
-  #default = "dev"
+default = "dev"
 
 }
 
 variable "business_unit" {
   type    = string
-  #default = "IT"
+  default = "IT"
 
 }
 
 variable "vnet_address_space" {
   type    = list(string)
-  #default = ["10.0.0.0/16"]
+  default = ["10.0.0.0/16"]
 
 }
 
 
 variable "subnet_name" {
   type    = string
-  #default = "mayur-snet"
+  default = "mayur-snet"
 
 }
 
@@ -35,6 +35,6 @@ variable "subnet_name" {
 
 variable "subnet_prefix" {
   type    = list(string)
-  #default = ["10.0.1.0/24"]
+  default = ["10.0.1.0/24"]
 
 }
